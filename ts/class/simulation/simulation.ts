7c796dfa6b8a5e732d8f9b0ce4a47661f29db951
haplo34 <haplonymous@gmail.com>
2021-05-11 14:21:06 +0200

    corrections sur rungekutta et simpson

--- ts/class/simulation/simulation.ts
+++ ts/class/simulation/simulation.ts
@@ -15,40 +15,41 @@ export abstract class Simulation {
 
     
 
-    /** Fourth order Runge-Kutta method.
+    /** Fourth order Runge-Kutta method for second order derivatives.
      * 
-     * Argument @derivative takes one of the second derivative defined in a special lib
+     * Argument @funct takes one of the second derivative defined in a special lib
      * depending on the type of the simulation.
      * 
      * Argument @h is the step
      * 
+     * The notations are @x_0 for x(0), @yp_0 for y'(0), etc.
+     * 
      * Returns a list of 2 values.
     */
-    public runge_kutta(derivative: (arg0: number, arg1: number) => any, x_0: number, y_0: number, h: number)
+    public runge_kutta(
+        funct: (arg0: number, arg1: number, arg2: number) => any, x_0: number, y_0: number, yp_0: number, h: number)
     {
-        let k_0 = derivative(x_0, y_0)
-        let k_1 = derivative(x_0 + h/2, y_0 + h/2 * k_0)
-        let k_2 = derivative(x_0 + h/2, y_0 + h/2 * k_1)
-        let k_3 = derivative(x_0 + h, y_0 + h * k_2)
+        let k_1 = funct(x_0, y_0, yp_0)
+        let k_2 = funct(x_0 + h/2, y_0 + h/2 * yp_0, yp_0 + h/2 * k_1)
+        let k_3 = funct(x_0 + h/2, y_0 + h/2 * yp_0 + h**2/4 * k_1, yp_0 + h/2 * k_2)
+        let k_4 = funct(x_0 + h, y_0 + h * yp_0 + h**2/2 * k_2, yp_0 + h * k_3)
 
-        let k = 1/6 * (k_0 + 2.0 * k_1 + 2.0 * k_2 + k_3)
+        let y_1 = y_0 + h * yp_0 + h**2/6 * (k_1 + k_2 + k_3)
+        let yp_1 = yp_0 + h/6 * (k_1 + 2*k_2 + 2*k_3 + k_4)
 
-        let x_1 = x_0 + h
-        let y_1 = y_0 + h * k
-
-        return [x_1, y_1]
+        return [y_1, yp_1]
     }
 
     /** Simple Simpson's rule implementation.
      * 
-     * Argument @derivative takes one of the second derivative defined in a special lib 
+     * Argument @funct takes one of the second derivative defined in a special lib 
      * depending on the type of the simulation.
      * 
      * Arguments @infimum and @supremum define a segment where @n is the number of points.
      * 
      * Returns a single value.
-     */
-    public simpson(derivative, infimum: number, supremum: number, n: number)
+    */
+    public simpson(funct: (arg0: any) => any, infimum: number, supremum: number, n: number)
     {
         let h = (supremum - infimum) / n;
         let x = [];
@@ -57,7 +58,7 @@ export abstract class Simulation {
         for (let i=0; i<=n; i++)
         {  
             x[i] = infimum + i * h;
-            y[i] = derivative(x[i]);
+            y[i] = funct(x[i]);
         }
         let res = 0;
         for (let i=0; i<=n; i++)
@@ -66,7 +67,6 @@ export abstract class Simulation {
             else if (i%2 != 0)  { res += 4 * y[i]; }
             else                { res += 2 * y[i]; }
         }
-        res = res * (h / 3);
-        return res;
+        return res * h/3;
     }
 }
