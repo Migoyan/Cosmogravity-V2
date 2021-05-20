83ba1d766bcbee878987e2ee27b6549cec4c3ef0
Migoyan <arthurtx88@gmail.com>
2021-05-20 17:41:11 +0200

    Added methods to simulation_universe
    Added methods to compute time as a function of z and also luminosity, metric and angular distances. Changed apparent_luminosity method name to brightness

--- ts/class/simulation/simulation_universe.ts
+++ ts/class/simulation/simulation_universe.ts
@@ -16,6 +16,8 @@ import { Simulation } from "./simulation";
  * 
  * methods names :
  * @method modify_dark_energy
+ * @method modify_constants
+ * @method runge_kutta_universe_2
  * @method calcul_omega_r
  * @method calcul_omega_k
  * @method Y
@@ -23,18 +25,20 @@ import { Simulation } from "./simulation";
  * @method F
  * @method get_interval_a
  * @method compute_a_tau
- * @method cosmologic_shift
+ * @method time
  * @method universe_age
  * @method duration
  * @method metric_distance
  * @method luminosity
  * @method luminosity_distance
  * @method angular_diameter_distance
- * @method apparent_magnitude
+ * @method brightness
  * @method apparent_diameter
  * @method integral_duration
  * @method integral_duration_substituated
  * @method integral_distance
+ * @method equa_diff_a
+ * @method equa_diff_time
  */
 export class Simulation_universe extends Simulation {
 
@@ -195,13 +199,63 @@ export class Simulation_universe extends Simulation {
      * @param step The step of computation
 	 * @param x_0 x_point where the calcul start
 	 * @param y_0 initial value of y at x_0
-	 * @param initial value of the derivative of y at x_0
+	 * @param interval Array containing [xmin, xmax]
+	 * @param funct function or method that define the equation to resolve, your function has to accept 2 numbers and return a number
+     * 
+     * @returns [step: number, x: number[], y:number[]].
+     */
+	 public runge_kutta_universe_1(
+		step: number,
+		x_0: number = 0,
+		y_0: number = 1,
+		funct: (x: number, y: number) => number,
+		interval: number[] = [0, 5],
+	)
+    {
+		// Init parameter
+		let x: number[] = [x_0];
+		let y: number[] = [y_0];
+		
+		// Computation loops
+		// Computing with a positive step, i increments the array
+		let i = 0;
+		let result_runge_kutta: number[];
+		while (interval[0] <= x[i] && x[i] < interval[1]) {
+			result_runge_kutta = this.runge_kutta_equation_order1(step, x[i], y[i], funct);
+			x.push(result_runge_kutta[0]);
+			y.push(result_runge_kutta[1]);
+			i++;
+		}
+		
+
+		/*
+			Computing with a negative step,
+			since we decrease the value of x we add the elements at the beginning of the arrays,
+			so for each step we take the first element of the array to compute the next one.
+		*/
+		while (interval[0] <= x[0] && x[0] < interval[1]) {
+			result_runge_kutta = this.runge_kutta_equation_order1(-step, x[0], y[0], funct);
+			x.unshift(result_runge_kutta[0]);
+			y.unshift(result_runge_kutta[1]);
+			i++;
+		}
+
+		return {x: x, y: y};
+	}
+
+	/** 
+	 * Fourth order Runge-Kutta method for second order derivatives for universe computation.
+     * 
+     * @param step The step of computation
+	 * @param x_0 x_point where the calcul start
+	 * @param y_0 initial value of y at x_0
+	 * @param yp_0 initial value of the derivative of y at x_0
 	 * @param interval Array containing [ymin, ymax]
 	 * @param funct function or method that define the equation to resolve, your function has to accept 3 numbers and return a number
      * 
      * @returns [step: number, x: number[], y:number[], yp: number[]].
      */
-	public runge_kutta_universe(
+	public runge_kutta_universe_2(
 		step: number,
 		x_0: number = 0,
 		y_0: number = 1,
@@ -220,7 +274,7 @@ export class Simulation_universe extends Simulation {
 		let i = 0;
 		let result_runge_kutta: number[];
 		while (interval[0] <= y[i] && y[i] < interval[1]) {
-			result_runge_kutta = this.runge_kutta(step, x[i], y[i], yp[i], funct);
+			result_runge_kutta = this.runge_kutta_equation_order2(step, x[i], y[i], yp[i], funct);
 			x.push(result_runge_kutta[0]);
 			y.push(result_runge_kutta[1]);
 			yp.push(result_runge_kutta[1]);
@@ -234,7 +288,7 @@ export class Simulation_universe extends Simulation {
 			so for each step we take the first element of the array to compute the next one.
 		*/
 		while (interval[0] <= y[0] && y[0] < interval[1]) {
-			result_runge_kutta = this.runge_kutta(-step, x[0], y[0], yp[0], funct);
+			result_runge_kutta = this.runge_kutta_equation_order2(-step, x[0], y[0], yp[0], funct);
 			x.unshift(result_runge_kutta[0]);
 			y.unshift(result_runge_kutta[1]);
 			yp.unshift(result_runge_kutta[1]);
@@ -322,32 +376,30 @@ export class Simulation_universe extends Simulation {
 	}
 
 	/**
-	 * Right part of the differential equation of a(tau) designed to be used in runge_kutta_universe method
-	 * @param tau time
-	 * @param a function a(t)
-	 * @param da derivative of a(t)
-	 * @returns result of the right part\
-	 * Note: tau and da are not used but have to be defined for this method to be accepted in the runge_kutta method of simulation class
-	 */
-	public equa_diff_a(tau: number = 0, a: number, da: number = 0): number {
-		return -(this.calcul_omega_r() / (a**2)) -
-		0.5 * this.calcul_omega_r() / (a**2) +
-		this.get_dark_energy().parameter_value * (a * this.Y(a) + (a**2) *  this.dY(a)/ 2);
-	}
-
-	/**
 	 * compute_a_tau in point 
 	 * @param step Computation step
 	 */
 	public compute_a_tau(step: number) {
-		return this.runge_kutta_universe(step, 0, 1, 1, this.equa_diff_a, this.get_interval_a());
+		return this.runge_kutta_universe_2(step, 0, 1, 1, this.equa_diff_a, this.get_interval_a());
 	}
 
 	/**
-	 * Compute the cosmologic shift z
+	 * Compute the time as a function of the cosmologic shift
+	 * @param n number of computation points
+	 * @param zmin
+	 * @param zmax
+	 * @returns
 	 */
-	public cosmologic_shift() {
+	public time(n: number, zmin: number, zmax: number) {
+		let step: number = (zmax - zmin) / n;
+		let time_zmin: number;
+		try {
+			time_zmin = this.duration(0, zmin);
+		} catch (e) {
+			return e;
+		}
 		
+		return this.runge_kutta_universe_1(step, zmin, time_zmin, this.equa_diff_time, [zmin, zmax]);
 	}
 
 	/**
@@ -372,8 +424,8 @@ export class Simulation_universe extends Simulation {
 	 * @returns error if z_1 or z_2 < -1, duration if both value are accepted.
 	 */
 	public duration(z_1: number, z_2: number) {
-		if ( z_1 < -1 || z_2 < -1 ) return {
-			error: "Cosmologic shift z cannot be lower than -1"
+		if ( z_1 < -1 || z_2 < -1 ) {
+			throw new Error("Cosmologic shift z cannot be lower than -1");
 		}
 
 		let duration: number;
@@ -418,7 +470,8 @@ export class Simulation_universe extends Simulation {
 	}
 
 	/**
-	 * 
+	 * @param z Cosmologic shift
+	 * @param distance_metric
 	 */
 	public angular_diameter_distance(z: number, distance_metric?: number) {
 		let distance: number;
@@ -428,7 +481,7 @@ export class Simulation_universe extends Simulation {
 			distance = distance_metric;
 		}
 
-		return distance / (1 + z)
+		return distance / (1 + z);
 	}
 
 	/**
@@ -441,12 +494,12 @@ export class Simulation_universe extends Simulation {
 	}
 
 	/**
-	 * Compute the apparant magnitude of an object situated at a cosmologic redshit z
+	 * Compute the brightness of an object situated at a cosmologic redshit z
 	 * @param z Cosmologic shift
 	 * @param luminosity self explanatory
 	 * @param distance_metric optionnal parameters for optimisation (permit you to pass an already calculated distances for optimisation)
 	 */
-	public apparent_magnitude(z: number, luminosity: number, distance_metric?: number): number {
+	public brightness(z: number, luminosity: number, distance_metric?: number): number {
 		let distance: number;
 		if (distance_metric === undefined) {
 			distance = this.metric_distance(z);
@@ -501,5 +554,31 @@ export class Simulation_universe extends Simulation {
 	 */
 	public integral_distance(x : number): number {
 		return 1 / Math.sqrt(this.F(x));
+	}
+
+	/**
+	 * Right part of the differential equation of a(tau) designed to be used in runge_kutta_universe_2 method
+	 * @param tau time
+	 * @param a function a(t)
+	 * @param da derivative of a(t)
+	 * @returns result of the right part\
+	 * Note: tau and da are not used but have to be defined for this method to be accepted in the runge_kutta_equation_order2 method of simulation class
+	 */
+	 public equa_diff_a(tau: number = 0, a: number, da: number = 0): number {
+		return -(this.calcul_omega_r() / a**2) -
+		0.5 * this.calcul_omega_r() / a**2 +
+		this.get_dark_energy().parameter_value * (a * this.Y(a) + a**2 *  this.dY(a)/ 2);
+	}
+
+	/**
+	 * Right part of the differential equation of t(z) designed to be used in runge_kutta_universe_1 method
+	 * @param z Cosmologic shift
+	 * @param t function time t(z)
+	 * @returns result of the right part\
+	 * Note: t is not used but has to be defined for this method to be accepted in the runge_kutta_equation_order1 method of simulation class
+	 */
+	public equa_diff_time(z: number, t: number = 0) {
+		let H0_si: number = this.get_hubble_cst() * 1e3 / ((AU * (180 * 3600)) / Math.PI * 1e6);
+		return 1 / (H0_si * (1 + z) * Math.sqrt(this.F(z)));
 	}
 }
