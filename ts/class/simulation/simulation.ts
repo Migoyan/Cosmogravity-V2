d2b1c5ff0b5b97eee76bfee150bead9d7ccfbafa
haplo34 <haplonymous@gmail.com>
2021-06-02 18:07:21 +0200

    new correction bug simulation

--- ts/class/simulation/simulation.ts
+++ ts/class/simulation/simulation.ts
@@ -30,7 +30,7 @@ export abstract class Simulation {
 	//---------------------- Methods -----------------------
 
 	
-    /**
+      /**
      * Fourth order Runge-Kutta method for first order derivatives.
      * 
      * @param step The step of computation
@@ -41,16 +41,17 @@ export abstract class Simulation {
      * @returns [x_1, y_1], value of the next point of computation
      */
     protected runge_kutta_equation_order1(
+        Simu: any,
         step: number,
         x_0: number,
         y_0: number,
-        funct: (x: number, y: number) => number
+        funct: (Simu: any, x: number, y: number) => number
     ): number[]
     {
-        let k_1 = funct(x_0, y_0);
-        let k_2 = funct(x_0 + step/2, y_0 + step/2 * k_1);
-        let k_3 = funct(x_0 + step/2, y_0 + step/2 * k_2);
-        let k_4 = funct(x_0 + step, y_0 + step * k_3);
+        let k_1 = funct(Simu, x_0, y_0);
+        let k_2 = funct(Simu, x_0 + step/2, y_0 + step/2 * k_1);
+        let k_3 = funct(Simu, x_0 + step/2, y_0 + step/2 * k_2);
+        let k_4 = funct(Simu, x_0 + step, y_0 + step * k_3);
 
 		let x_1 = x_0 + step;
 		let y_1 = y_0 + step * ((1/6)*k_1 + (1/3)*k_2 + (1/3)*k_3 + (1/6)*k_4);
@@ -59,7 +60,7 @@ export abstract class Simulation {
     }
 
 
-    /** 
+     /** 
      * Fourth order Runge-Kutta method for second order derivatives.
      * 
      * @param step The step of computation
@@ -68,20 +69,21 @@ export abstract class Simulation {
 	 * @param dy_0 initial value of the derivative of y
 	 * @param funct function or method that define the equation to resolve
      * 
-     * @returns [x_1, y_1, dy_1], value of the next point of computation
+     * @returns [x_1, y_1, yp_1], value of the next point of computation
      */
     protected runge_kutta_equation_order2(
+        Simu: any,
         step: number,
         x_0: number,
         y_0: number,
         dy_0: number,
-        funct: (x: number, y: number, dy: number) => number
+        funct: (Simu: any, x: number, y: number, dy: number) => number
     ): number[]
     {
-		let k_1 = funct(x_0, y_0, dy_0);
-        let k_2 = funct(x_0 + step/2, y_0 + step/2 * dy_0, dy_0 + step/2 * k_1);
-        let k_3 = funct(x_0 + step/2, y_0 + step/2 * dy_0 + step**2/4 * k_1, dy_0 + step/2 * k_2);
-        let k_4 = funct(x_0 + step, y_0 + step * dy_0 + step**2/2 * k_2, dy_0 + step * k_3);
+		let k_1 = funct(Simu, x_0, y_0, dy_0);
+        let k_2 = funct(Simu, x_0 + step/2, y_0 + step/2 * dy_0, dy_0 + step/2 * k_1);
+        let k_3 = funct(Simu, x_0 + step/2, y_0 + step/2 * dy_0 + step**2/4 * k_1, dy_0 + step/2 * k_2);
+        let k_4 = funct(Simu, x_0 + step, y_0 + step * dy_0 + step**2/2 * k_2, dy_0 + step * k_3);
 
 		let x_1 = x_0 + step;
 		let y_1 = y_0 + step * dy_0 + step**2/6 * (k_1 + k_2 + k_3);
